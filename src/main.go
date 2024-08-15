9378015a2c3a64e246a90a1b1017ee197e200fbe
zhangzan <zhangzan@tal.com>
2024-08-15 11:19:52 +0800

    fix: 模拟打完tag后的修复bug场景

--- src/main.go
+++ src/main.go
@@ -1,11 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func helloWorldInMain() {
+	fmt.Println("Hello World")
+	fmt.Println("Hello World")
+	fmt.Println("Hello World")
+	fmt.Println("Hello World after tag v1.0.0")
+}
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Hello World")
-	fmt.Println("Hello World")
+	helloWorldInMain()
 }
