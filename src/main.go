92694a8f1cc3780a7c3d2c14a80aca558c7fad50
zhangzan <zhangzan@tal.com>
2024-08-15 11:07:59 +0800

    fet: git工作流掌握与学习4

--- src/main.go
+++ src/main.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	lambdafunction "basicLearning/src/lambdafunction"
 	"fmt"
 )
-
-func usingLambda() {
-	for i := 0; i < 10; i++ {
-		lambdafunction.UsingLambda()
-	}
-
-}
 
 func main() {
 	fmt.Println("Hello World")
