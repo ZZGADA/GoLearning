5b406ec4496c74b98e205f97616c5fefe55aa771
zhangzan <zhangzan@tal.com>
2024-08-18 11:17:37 +0800

    fit: go整合gorm，实现数据库的增删改查，

--- main.go
+++ main.go
@@ -2,22 +2,65 @@ package main
 
 import (
 	"basicLearning/src/config"
-	ginstudy "basicLearning/src/ginStudy"
+	"basicLearning/src/connectToMysql"
 	"fmt"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"path"
+	"reflect"
+	"runtime"
 )
 
-func temp() {
-
+var LogConf = struct {
+	Dir     string `yaml:"dir"`
+	Name    string `yaml:"name"`
+	Level   string `yaml:"level"`
+	MaxSize int    `yaml:"max_size"`
+}{
+	Dir:     "./logs",
+	Name:    "yourlogname.log",
+	Level:   "trace",
+	MaxSize: 100,
 }
 
-func main() {
-	nacosConfig := config.Load()
-	nacosConfigCenter := ginstudy.InitNacosConfigCenter(nacosConfig)
+func nacosTemp(nacosConfigCenter config_client.IConfigClient) {
 	val, err := nacosConfigCenter.GetConfig(vo.ConfigParam{
 		DataId: "basic-learning-go.yaml",
 		Group:  "GO",
 	})
 	fmt.Println("hello world")
 	fmt.Println(val, err)
+}
+
+func mysqlTemp(nacosConfigCenter config_client.IConfigClient) {
+	// 定义一个切片
+	var users []connectToMysql.TransactionStudy
+	connectToMysql.ConnectToMysql(nacosConfigCenter)
+	// 查询主键 in （2，4，5）中的
+	connectToMysql.MysqlClient.Find(&users, []int{2, 4, 5})
+	fmt.Println(reflect.TypeOf(users).Kind())
+	for _, val := range users {
+		fmt.Println(val)
+	}
+
+	//var user1 connectToMysql.TransactionStudy
+	connectToMysql.MysqlClient.Find(&users, "age > ? and address = ?", 22, "HN")
+	for _, val := range users {
+		fmt.Printf("%#v", val)
+	}
+
+}
+
+func getProjectRoot() {
+	// 通过Caller获取当前执行文件的目录地址
+	// 然后获取父级目录
+	_, filename, _, _ := runtime.Caller(0)
+	root := path.Dir(filename)
+	fmt.Println(root)
+}
+
+func main() {
+	nacosConfig := config.Load()
+	fmt.Println(nacosConfig)
+
 }
--- src/connectToMysql/connectMysql.go
+++ src/connectToMysql/connectMysql.go
@@ -1,6 +1,98 @@
 package connectToMysql
 
-// ConnectToMysql // 连接数据库
-func ConnectToMysql() {
+import (
+	"fmt"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"gopkg.in/yaml.v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"log"
+	"time"
+)
 
+type TransactionStudy struct {
+	Id      int64  `mapstructure:"id" gorm:"primarykey"`
+	Name    string `mapstructure:"name"`
+	Age     int32  `mapstructure:"age"`
+	Address string `mapstructure:"address"`
+}
+
+func NewTransactionStudy(id int64, name string, age int32, address string) *TransactionStudy {
+	return &TransactionStudy{Id: id, Name: name, Age: age, Address: address}
+}
+
+type Server struct {
+	Port int32 `yaml:"port" mapstructure:"port"`
+}
+type DataBases struct {
+	Mysql MySQLConfig `yaml:"mysql" mapstructure:"mysql"`
+}
+
+type Config struct {
+	DataBases DataBases `yaml:"databases" mapstructure:"databases"`
+	Server    Server    `yaml:"server" mapstructure:"server"`
+}
+
+type MySQLConfig struct {
+	Username string `yaml:"username" mapstructure:"username"`
+	Password string `yaml:"password" mapstructure:"password"`
+	Ip       string `yaml:"ip" mapstructure:"ip"`
+	Port     int32  `yaml:"port" mapstructure:"port"`
+	Database string `yaml:"database" mapstructure:"database"`
+}
+
+// TableName 指定表名
+func (TransactionStudy) TableName() string {
+	return "transaction_study"
+}
+
+// MysqlClient is mysql client
+var MysqlClient *gorm.DB
+
+const mysqlConnectStr string = "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+
+// ConnectToMysql // 连接数据库
+func ConnectToMysql(nacosClient config_client.IConfigClient) {
+	mysqlConfigYaml, err := nacosClient.GetConfig(vo.ConfigParam{
+		DataId: "paper-upload-project",
+		Group:  "go",
+	})
+	//fmt.Println(mysqlConfigYaml)
+
+	if err != nil {
+		log.Fatalf("Parse config.mysql segment error: %s\n", err)
+	}
+	var config Config
+	yaml.Unmarshal([]byte(mysqlConfigYaml), &config)
+
+	//dsn格式 user:pass@tcp(ip:port)/dbname?charset=utf8mb4&parseTime=True&loc=Local
+	dsn := fmt.Sprintf(mysqlConnectStr,
+		config.DataBases.Mysql.Username,
+		config.DataBases.Mysql.Password,
+		config.DataBases.Mysql.Ip,
+		config.DataBases.Mysql.Port,
+		config.DataBases.Mysql.Database)
+
+	mysqlClient, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "",   // 表前缀
+			SingularTable: true, // 禁用表名复数
+		}})
+	if err != nil {
+		panic(err)
+	}
+
+	mysqlClientDB, _ := mysqlClient.DB()
+
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	mysqlClientDB.SetMaxIdleConns(10)
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	mysqlClientDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	mysqlClientDB.SetConnMaxLifetime(time.Hour)
+
+	MysqlClient = mysqlClient
 }
